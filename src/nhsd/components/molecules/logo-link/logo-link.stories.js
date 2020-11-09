75d34480ff65a12cf469723f90d805ea6f21e2da
Rob Pataki <robert.pataki@valtech.com>
2020-11-09 14:15:18 +0000

    [DW-1679] Add Logo Link molecule
    - Extract the global transition duration into a variable

---
+++ src/nhsd/components/molecules/logo-link/logo-link.stories.js
@@ -0,0 +1,45 @@
+// Load storybook config
+import * as sbConfig from '../../../../../.storybook/storybook-config.js';
+
+// Load template file
+import template from  './template.njk';
+// Load stylesheet file
+require('./_index.scss');
+
+const componentName = 'Logo link';
+const storyDescription = `${sbConfig.heading.lab}`;
+const sourceCode = `// Sass import \n@use "nhsd/components/molecules/logo-link";
+
+// HTML`;
+
+// Component defaults
+export default {
+  title: `${sbConfig.title.designSystem} / ${sbConfig.title.components} / ${sbConfig.title.molecules} / ${componentName}`,
+  parameters: {
+    docs: {
+      description: {
+        component: storyDescription
+      }
+    },
+    notes: `${sbConfig.heading.basicRules}`,
+  },
+};
+
+// Component template
+const Template = (args) => {
+  return template.render({ params: {...args} });
+};
+
+export const LabComponent = Template.bind({});
+LabComponent.storyName = sbConfig.title.lab;
+LabComponent.args = {
+  url: '#',
+  label: 'Home page',
+};
+LabComponent.parameters = {
+  docs: {
+    source: {
+      code: `${sourceCode}\n${LabComponent(LabComponent.args)}`,
+    },
+  },
+};
