b4e434b8baab0cbd6e3ff5cd1f73d2319b5426a5
Rob Pataki <robert.pataki@valtech.com>
2020-11-21 13:24:30 +0000

    Add Devices to Viewport addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -12,4 +12,43 @@ export const parameters = {
       { name: 'bright', value: '#fae100' },
     ],
   },
+  viewport: {
+    viewports: {
+      mobile: {
+        name: 'Mobile',
+        styles: {
+          width: '375px',
+          height: '667px',
+        },
+      },
+      tablet: {
+        name: 'Tablet',
+        styles: {
+          width: '1024px',
+          height: '768px',
+        },
+      },
+      desktop: {
+        name: 'Desktop',
+        styles: {
+          width: '1280px',
+          height: '1024px',
+        },
+      },
+      hd: {
+        name: 'HD',
+        styles: {
+          width: '1366px',
+          height: '768px',
+        },
+      },
+      fullHd: {
+        name: 'Full HD',
+        styles: {
+          width: '1920px',
+          height: '1080px',
+        },
+      },
+    },
+  },
 }
