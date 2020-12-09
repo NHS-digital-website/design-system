01c1d7bce3f7da865597437506eeda8c94f8b497
Rob Pataki <robert.pataki@valtech.com>
2020-12-09 19:10:32 +0000

    [DW-1838] Colour bar atom

---
+++ src/nhsd/components/atoms/colour-bar/colour-bar.stories.js
@@ -0,0 +1,39 @@
+// Load storybook config
+import * as sbConfig from '../../../../../.storybook/storybook-config.js';
+
+// Load template file
+import template from  './template.njk';
+// Load stylesheet file
+require('./_index.scss');
+
+const componentName = 'Colour bar';
+const storyDescription = `${sbConfig.heading.lab}`;
+const sourceCode = `// Sass import \n@use "nhsd/components/atoms/colour-bar";
+
+// HTML`;
+
+// Component defaults
+export default {
+  title: `${sbConfig.title.designSystem} / ${sbConfig.title.components} / ${sbConfig.title.atoms} / ${componentName}`,
+  parameters: {
+    docs: {
+      description: {
+        component: storyDescription
+      }
+    },
+  },
+};
+
+const Template = (args) => {
+  return template.render({ params: {...args} });
+};
+
+export const LabComponent = Template.bind({});
+LabComponent.storyName = sbConfig.title.lab;
+LabComponent.parameters = {
+  docs: {
+    source: {
+      code: `${sourceCode}\n${LabComponent()}`,
+    }
+  },
+};
