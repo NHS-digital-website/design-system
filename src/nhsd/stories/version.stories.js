af3cd9df55ade62748e51c888ca279ab57c66c67
Rob Pataki <robert.pataki@valtech.com>
2020-12-14 18:39:32 +0000

    [HK] Load fonts from NHS until NHSD S3 is more capable

--- src/nhsd/stories/version.stories.js
+++ src/nhsd/stories/version.stories.js
@@ -22,12 +22,12 @@ const Template = (args) => {
 
   const Title = document.createElement('h1');
   Title.classList.add('nhsd-t-heading-xl');
-  Title.innerText = `Latest version: v0.1.0`;
+  Title.innerText = `Latest version: v0.1.1`;
   div.appendChild(Title);
 
   const subTitle = document.createElement('h2');
   subTitle.classList.add('nhsd-t-heading-l');
-  subTitle.innerText = `Date: 13/12/2020`;
+  subTitle.innerText = `Date: 14/12/2020`;
   div.appendChild(subTitle);
 
   return div;
