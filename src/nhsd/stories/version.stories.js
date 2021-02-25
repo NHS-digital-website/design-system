ad1456bbad34e036a477632374dad7c2342c789b
Lee Jacobson <lee.jacobson1@nhs.net>
2021-02-25 12:12:11 +0000

    [DW-1940] A-Z nav updates

--- src/nhsd/stories/version.stories.js
+++ src/nhsd/stories/version.stories.js
@@ -23,7 +23,7 @@ const Template = () => {
 
   const Title = document.createElement('h1');
   Title.classList.add('nhsd-t-heading-xl');
-  Title.innerText = 'Latest version: v0.4.0.18';
+  Title.innerText = 'Latest version: v0.4.0.19';
   div.appendChild(Title);
 
   const subTitle = document.createElement('h2');
