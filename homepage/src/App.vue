6f641227e2321710a2c42dede80d0bd994fa2d8a
GeoffHayward <geoffrey.hayward@nhs.net>
2021-04-15 15:26:06 +0100

    [DW-000] Creates an auto-updating homepage

---
+++ homepage/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="app" >
+    <global-header />
+    <home-banner-hero />
+    <div class="nhsd-t-grid">
+      <div class="nhsd-t-row nhsd-!t-margin-top-8">
+        <div class="nhsd-t-col-3">
+          <div class="nhsd-m-sticky-navigation nhsd-!t-display-sticky nhsd-!t-display-sticky--offset-2"><span id="sticky-nav-header" class="nhsd-t-heading-xs nhsd-!t-margin-bottom-2">Page of contents</span>
+            <hr class="nhsd-a-horizontal-rule nhsd-a-horizontal-rule--size-xs nhsd-!t-margin-bottom-2"/>
+            <nav aria-labelledby="sticky-nav-header">
+              <ul>
+                <li class="nhsd-m-sticky-navigation__item" >
+                  <a class="nhsd-a-link" href="#top">Top of page</a>
+                </li>
+                <li class="nhsd-m-sticky-navigation__item" data-nav-content="documentation">
+                  <a class="nhsd-a-link" href="#documentation">Documentation</a>
+                </li>
+                <li class="nhsd-m-sticky-navigation__item" data-nav-content="content-delivery-network">
+                  <a class="nhsd-a-link" href="#content-delivery-network">Content Delivery Network</a>
+                </li>
+                <li class="nhsd-m-sticky-navigation__item" data-nav-content="node-package-manger">
+                  <a class="nhsd-a-link" href="#node-package-manger">NPM Distribution</a>
+                </li>
+                <li class="nhsd-m-sticky-navigation__item" data-nav-content="contact">
+                  <a class="nhsd-a-link" href="#contact">Feedback &amp; Contact</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div class="nhsd-t-col-9">
+          <div id="documentation">
+            <storybook-card :releases="releases" />
+          </div>
+          <hr class="nhsd-a-horizontal-rule" />
+          <div id="content-delivery-network">
+            <content-delivery-network-card :releases="releases" />
+          </div>
+          <hr class="nhsd-a-horizontal-rule" />
+          <div id="node-package-manger">
+            <node-package-manager-card />
+          </div>
+          <hr class="nhsd-a-horizontal-rule" />
+          <div id="contact">
+            <p>Please send all feedback and contact to <a class="nhsd-a-link" href="mailto:websitedevelopment@nhs.net">NHS Digital Web Team</a>.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import GlobalHeader from './components/GlobalHeader'
+import HomeBannerHero from '@/components/HomeBannerHero'
+import ContentDeliveryNetworkCard from './components/ContentDeliveryNetworkCard'
+import StorybookCard from './components/StorybookCard'
+import NodePackageManagerCard from '@/components/NodePackageManagerCard';
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  components: {
+    GlobalHeader,
+    HomeBannerHero,
+    ContentDeliveryNetworkCard,
+    StorybookCard,
+    NodePackageManagerCard
+  },
+  data() {
+    return {
+      releases: []
+    }
+  },
+  created() {
+    axios.get("https://api.github.com/repos/NHS-digital-website/nhsd-frontend/releases?page=1&per_page=9")
+      .then(response => this.releases = response.data)
+  },
+  mounted() {
+    const el = document.createElement("script");
+    el.type = 'text/javascript';
+    el.async = true;
+    el.src = "https://design-system.digital.nhs.uk/cdn/latest/scripts/nhsd-frontend.js";
+    document.head.append(el);
+  }
+}
+</script>
---
+++ homepage/src/components/ContentDeliveryNetworkCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h2 class="nhsd-t-heading-xl nhsd-!t-margin-bottom-6 ">Content Delivery Network</h2>
+    <p>NHS teams are very welcome to using our design system via our content delivery network.</p>
+    <article class="nhsd-m-card">
+      <a href="https://design-system.digital.nhs.uk/cdn/latest/stylesheets/nhsd-frontend.css" class="nhsd-a-box-link " target="_blank" rel="external"  aria-label="About NHS Digital - More about what we do (external link, opens in a new tab)" >
+        <div class="nhsd-a-box nhsd-a-box--bg-light-grey">
+          <div class="nhsd-m-card__content_container">
+            <div class="nhsd-m-card__content-box">
+              <div class="nhsd-m-card__tag-list">
+                <span class="nhsd-a-tag nhsd-a-tag--bg-dark-grey">Currently at <span v-if="this.releases[0]">{{ this.releases[0].tag_name }}</span></span>
+              </div>
+              <p class="nhsd-t-heading-s">Stay up to date</p>
+              <p class="nhsd-t-body-s">By using the latest distribution you are automatically keeping your project up to date.</p>
+            </div>
+            <div class="nhsd-m-card__button-box">
+              <span class="nhsd-a-button">
+                <span class="nhsd-a-button__label">Latest CSS</span>
+              </span></div>
+          </div>
+        </div>
+      </a>
+    </article>
+    <p>If your project needs to keep to a locked version.</p>
+    <div class="nhsd-m-table nhsd-t-body">
+      <table data-responsive data-no-sort>
+        <thead>
+            <tr>
+              <th>Published</th>
+              <th>CSS</th>
+              <th>JavaScript</th>
+              <th>Download</th>
+              <th>GitHib Release</th>
+            </tr>
+          </thead>
+          <tbody >
+            <tr v-for="release in releases" :key="release.id">
+              <td>{{ release.published_at }}</td>
+              <td><a class="nhsd-a-link" :href="'https://design-system.digital.nhs.uk/cdn/'+ release.tag_name +'/stylesheets/nhsd-frontend.css'">Hosted</a></td>
+              <td><a class="nhsd-a-link" :href="'https://design-system.digital.nhs.uk/cdn/'+ release.tag_name +'/scripts/nhsd-frontend.js'">Hosted</a></td>
+              <td><a class="nhsd-a-link" :href="release.assets[0].browser_download_url">{{ release.assets[0].name }}</a></td>
+              <td><a class="nhsd-a-link" :href="release.html_url">{{ release.tag_name }}</a></td>
+            </tr>
+          </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContentDeliveryNetworkCard",
+  props: [
+    'releases'
+  ]
+}
+</script>
