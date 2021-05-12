5b81ce27d90cbc9748b4e900cc79ce93a7f55edc
sidullah <sid.ullah@answerdigital.com>
2021-05-12 13:14:27 +0100

    [DW-000] Fix expander compound styling and change to pure css solution

--- src/nhsd/script-core/init.js
+++ src/nhsd/script-core/init.js
@@ -4,7 +4,6 @@ import NHSDGlobalHeader from '../components/organisms/global-header/global-heade
 import NHSDCodeViewer from '../components/organisms/code-viewer/code-viewer';
 import NHSDStickyNavigation from '../components/molecules/sticky-navigation/sticky-navigation';
 import NHSDFilterMenuSection from '../components/molecules/filter-menu-section/filter-menu-section';
-import NHSDExpander from '../components/molecules/expander/expander';
 import NHSDTable from '../components/molecules/table/table';
 import NHSDTabs from '../components/molecules/tabs/tabs';
 import NHSDNotificationBanner from '../components/molecules/notification-banner/notification-banner';
@@ -34,7 +33,6 @@ export default function init() {
     initComponent('.nhsd-o-code-viewer', NHSDCodeViewer);
     initComponent('.nhsd-m-sticky-navigation', NHSDStickyNavigation);
     initComponent('.nhsd-m-filter-menu-section', NHSDFilterMenuSection);
-    initComponent('.nhsd-m-expander', NHSDExpander);
     initComponent('.nhsd-m-table', NHSDTable);
     initComponent('.nhsd-m-tabs', NHSDTabs);
     initComponent('.nhsd-m-notification-banner', NHSDNotificationBanner);
