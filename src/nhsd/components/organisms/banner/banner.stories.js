3ebba642e10d026a58d7d60b32050555cf8428fc
Lee Jacobson <lee.jacobson1@nhs.net>
2021-08-06 14:28:42 +0100

    [DW-2462] hero refactoring

--- src/nhsd/components/organisms/banner/banner.stories.js
+++ src/nhsd/components/organisms/banner/banner.stories.js
@@ -13,9 +13,7 @@ const componentName = 'Banner';
 const storyDescription = `${sbConfig.heading.lab}
 
 ### Components used
-- <a href="/docs/design-system-components-atoms-box--lab-component">Box atom</a>
 - <a href="/docs/design-system-components-atoms-button--lab-component">Button atom</a>
-- <a href="/docs/design-system-components-atoms-digiblocks--lab-component">Digiblocks atom</a>
 - <a href="/docs/design-system-components-molecules-button-nav--lab-component">Button Nav molecule</a>
 
 ### Tokens used
@@ -26,50 +24,10 @@ const storyDescription = `${sbConfig.heading.lab}
 
 ${sbConfig.heading.basicRules}:
 
-- Use the canvas view to see the component variants without unintendedly breaking
-- External button atom <a href="/docs/design-system-components-atoms-button--link-button#link-button">rules</a> apply for external links!
-- **There are 2 types of the hero organism:**
-  - ***Default hero*** (Coloured background, title, text, optional button and optional image) - used on article pages as a banner;
-  - ***Featured hero*** (Coloured background of content column; title, text, optional button; picture) - used on the home page
-- Default hero is \`nhsd-o-hero\`. Please note that this version is only **Work In Progress and not suitable ready to be used in production yet!**
-- Featured hero is \`nhsd-o-hero-feature\`, but it is using the same building blocks and styling as regular hero.
-- Only featured hero handles **mirroring** and **accenting**
-- Accented hero stays 12 cols wide on mobile, non-accented featured hero goes 11 columns on mobile, and gets rounded edges
-- The content of the heros is completely up to the implementor, but here are some recommendations (a.k.a. common usecases):
-  - **Contents**:
-    - Title token - for featured hero it's likely to be a \`<span>\` or \`<div>\`, for regular hero it's a \`<h1>\` or \`<h2>\`
-    - Body text token - use a \`<p>\`
-    - Button atom (optional) - use \`<a>\` element
-    - Colour bar atom - for featured and accented hero it's mandatory, for regular featured it's optional
-    - Image atom
-  - **Variants**:
-    - ***Mirrored***: Image on the left, contents on the right
-    - ***Accented***: Edges stay sharp and uses 12 columns on mobile
-  - **Additional rules**
-    - Use the background clour tokens to override the hero's background colour (\`.nhsd-!t-bg-light-green\`).
-    - Accented Featured hero uses a Square image atom (\`.nhsd-a-image--square\`); XXL heading (\`.nhsd-t-heading-xxl\`)
-    - Regular Featured hero uses a regular image atom (\`.nhsd-a-image--round-corners\`); Large heading (\`.nhsd-t-heading-l\`)
-    - Contents are vertically centred in all cases.
-    - Featured hero uses line clamping between tablet and desktop breakpoints to avoid breaking if text is too long.
-    - The height of the featured hero is determined by the image used: image ratio is 16:9 for non-accented and 1:1 for accented variant.
-
-### Line clamping rules - Featured type
-
-Heading is always clamped to max. 3 lines.
-
-|Device type|Shorthand|Variant|Body text (max. lines)|
-|---|---|---|---|---|
-|Mobile|**XS**|Accented|7|
-|Mobile|**XS**|Regular|7|
-|Tablet|**S**|Accented|4|
-|Tablet|**S**|Regular|3|
-|Desktop|**M**|Accented|8|
-|Desktop|**M**|Regular|4|
-|Wide|**L**|Accented|10|
-|Wide|**L**|Regular|4|
-|Extra Wide|**XL**|Accented|12|
-|Extra Wide|**XL**|Regular|4|`;
-const sourceCode = `// Sass import \n@use "nhsd/components/organisms/hero";
+- The banner component is a banner which is intended to be used in document content. 
+  - For header banners see the <a href="/docs/design-system-components-organisms-hero--lab-component">hero component</a>
+  - Banners turn into cards at mobile viewports`;
+const sourceCode = `// Sass import \n@use "nhsd/components/organisms/banner";
 
 // HTML`;
 
