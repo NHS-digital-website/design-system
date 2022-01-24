322af3c7a88bf4317a08d845a31c9db77ec5a802
Lee Jacobson <lee.jacobson1@nhs.net>
2022-01-24 12:04:53 +0000

    [DW-2443] Mega menu

--- src/nhsd/components/organisms/global-header/global-header.js
+++ src/nhsd/components/organisms/global-header/global-header.js
@@ -1,49 +1,104 @@
-/* global document window */
+/* global document */
 
-import breakpoints from '../../../scss-core/settings/_media-queries.scss';
+import nhsd from '@/nhsd';
+import scrollLock from '@/helpers/scroll-lock';
+import FocusTrapper from '@/helpers/FocusTrapper';
 
 export default class NHSDGlobalHeader {
   constructor(componentEl) {
     if (!componentEl) return null;
     this.hostEl = componentEl;
     this.html = document.querySelector('html');
-    this.body = document.body;
     this.logoEl = this.hostEl.querySelector('#nhsd-global-header__logo');
     this.menuBarEl = this.hostEl.querySelector('#nhsd-global-header__menu');
     this.searchInput = this.hostEl.querySelector('#query');
+    this.megaMenu = this.hostEl.querySelector('.nhsd-o-global-header__mega-menu');
     this.menuBarActive = false;
-
-    this.cachedProps = {
-      html: {
-        overflow: this.html.style.overflow,
-        height: this.html.style.height,
-      },
-      body: {
-        overflow: this.html.style.overflow,
-        height: this.html.style.height,
-      },
-    };
+    this.focusTrapper = new FocusTrapper(this.megaMenu);
 
     this.initMenuBar();
     this.initSearchBar();
+    this.#initMegaMenu();
+  }
 
-    // Handle resize event
-    window.addEventListener('resize', () => {
-      // Close the menu on desktops
-      if (window.innerWidth >= breakpoints['nhsd-breakpoint-m'] && this.menuBarActive) {
-        this.closeMenuBar();
+  #openMegaMenu(menuId) {
+    this.#closeMegaMenu();
+    this.closeMenuBar();
+    const megaMenu = this.megaMenu.querySelector(`#${menuId}`);
+    if (megaMenu) {
+      megaMenu.classList.add('nhsd-o-global-header__mega-menu-content--active');
+    }
+    const megaMenuButton = document.querySelector(`.nhsd-a-menu-item[data-mega-menu="${menuId}"]`);
+    if (megaMenuButton) {
+      megaMenuButton.classList.add('nhsd-a-menu-item--active');
+      megaMenuButton.setAttribute('aria-expanded', true);
+    }
+    this.hostEl.classList.add('nhsd-o-global-header--mega-menu-active');
+    scrollLock(true, 'mobile');
+    this.focusTrapper.focus(this.megaMenu);
+  }
+
+  #closeMegaMenu() {
+    this.hostEl.classList.remove('nhsd-o-global-header--mega-menu-active');
+    const megaMenuButtons = document.querySelectorAll('.nhsd-a-menu-item');
+    megaMenuButtons.forEach((megaMenuButton) => megaMenuButton.classList.remove('nhsd-a-menu-item--active'));
+    scrollLock(false, 'mobile');
+
+    const megaMenuContentEls = this.megaMenu.querySelectorAll('.nhsd-o-global-header__mega-menu-content--active');
+    megaMenuContentEls.forEach((megaMenuContentEl) => megaMenuContentEl.classList.remove('nhsd-o-global-header__mega-menu-content--active'));
+
+    if (megaMenuContentEls.length > 0 && megaMenuContentEls[0].id) {
+      const menuId = megaMenuContentEls[0].id;
+
+      const megaMenuButton = document.querySelector(`.nhsd-a-menu-item[data-mega-menu="${menuId}"]`);
+      if (megaMenuButton) {
+        megaMenuButton.setAttribute('aria-expanded', false);
+        megaMenuButton.focus();
+      }
+    }
+  }
+
+  #initMegaMenu() {
+    const megaMenuButtons = document.querySelectorAll('[data-mega-menu]');
+    megaMenuButtons.forEach((megaMenuButton) => {
+      nhsd(megaMenuButton).on('click', () => {
+        const megaMenuId = megaMenuButton.dataset.megaMenu;
+        nhsd(this.hostEl).trigger('megamenu-toggle', megaMenuId);
+        return false;
+      });
+    });
+
+    const megaMenuCloseButtons = document.querySelectorAll('[data-mega-menu-close]');
+    megaMenuCloseButtons.forEach((megaMenuButton) => {
+      nhsd(megaMenuButton).on('click', () => {
+        nhsd(this.hostEl).trigger('megamenu-close');
+        return false;
+      });
+    });
+
+    nhsd(this.hostEl).on('megamenu-toggle', (e) => {
+      if (this.hostEl.classList.contains('nhsd-o-global-header--mega-menu-active')) {
+        this.#closeMegaMenu();
+      } else {
+        this.#openMegaMenu(e.detail);
       }
     });
+
+    nhsd.on('keyup.global-header', (e) => {
+      if (e.key === 'Escape') {
+          nhsd(this.hostEl).trigger('megamenu-close');
+      }
+    });
+
+    nhsd(this.hostEl).on('megamenu-open', (e) => this.#openMegaMenu(e.detail));
+
+    nhsd(this.hostEl).on('megamenu-close', () => this.#closeMegaMenu());
   }
 
   initMenuBar() {
-    if (!this.hostEl) return;
     this.menuButtonEl = this.hostEl.querySelector('#nhsd-global-header__menu-button');
     if (!this.menuButtonEl) return;
-    this.menuButtonEl.addEventListener('click', (event) => {
-      event.preventDefault();
-      event.stopPropagation();
-
+    nhsd(this.menuButtonEl).on('click', () => {
       if (!this.menuBarActive) {
         this.openMenuBar();
       } else {
@@ -53,14 +108,20 @@ export default class NHSDGlobalHeader {
       if (this.searchBarActive) {
         this.closeSearchBar();
       }
+
+      return false;
     });
 
     this.menuCloseButtonEl = this.hostEl.querySelector('#nhsd-global-header__menu-close-button');
-    this.menuCloseButtonEl.addEventListener('click', (event) => {
-      event.preventDefault();
-      event.stopPropagation();
-
+    nhsd(this.menuCloseButtonEl).on('click', () => {
       this.closeMenuBar();
+      return false;
+    });
+
+    const openMenuButtons = this.hostEl.querySelectorAll('[data-menu-open]');
+    nhsd(Array.from(openMenuButtons)).on('click', () => {
+      this.openMenuBar();
+      return false;
     });
   }
 
@@ -68,10 +129,7 @@ export default class NHSDGlobalHeader {
     this.searchButtonEl = this.hostEl.querySelector('#nhsd-global-header__search-button');
     if (!this.searchButtonEl) return;
 
-    this.searchButtonEl.addEventListener('click', (event) => {
-      event.preventDefault();
-      event.stopPropagation();
-
+    nhsd(this.searchButtonEl).on('click', () => {
       if (!this.searchBarActive) {
         this.openSearchBar();
       } else {
@@ -81,18 +139,19 @@ export default class NHSDGlobalHeader {
       if (this.menuBarActive) {
         this.closeMenuBar();
       }
+
+      return false;
     });
 
     this.searchCloseButtonEl = this.hostEl.querySelector('#nhsd-global-header__search-close-button');
-    this.searchCloseButtonEl.addEventListener('click', (event) => {
-      event.preventDefault();
-      event.stopPropagation();
-
+    nhsd(this.searchCloseButtonEl).on('click', () => {
       this.closeSearchBar();
+      return false;
     });
   }
 
   openMenuBar() {
+    this.#closeMegaMenu();
     this.hostEl.classList.add('js-menu-active');
     this.menuBarActive = true;
     this.menuButtonEl.setAttribute('aria-expanded', true);
@@ -112,18 +171,17 @@ export default class NHSDGlobalHeader {
 
   openSearchBar() {
     this.hostEl.classList.add('js-search-active');
-    this.searchButtonEl.classList.add('js-active');
     this.searchButtonEl.blur();
     if (this.searchInput) this.searchInput.focus();
     this.searchButtonEl.setAttribute('aria-expanded', true);
     this.searchBarActive = true;
 
     this.setDocumentScrolling();
+    this.#closeMegaMenu();
   }
 
   closeSearchBar() {
     this.hostEl.classList.remove('js-search-active');
-    this.searchButtonEl.classList.remove('js-active');
     this.searchButtonEl.blur();
     this.searchButtonEl.setAttribute('aria-expanded', false);
     this.searchBarActive = false;
@@ -133,19 +191,9 @@ export default class NHSDGlobalHeader {
 
   setDocumentScrolling() {
     if (this.hostEl.classList.contains('js-menu-active') || this.hostEl.classList.contains('js-search-active')) {
-      this.html.style.overflow = 'hidden';
-      this.body.style.overflow = 'hidden';
-      this.html.style.height = '100%';
-      this.body.style.height = '100%';
+      scrollLock(true);
     } else {
-      this.html.style.overflow = this.cachedProps.html.overflow
-        ? this.cachedProps.html.overflow : null;
-      this.html.style.height = this.cachedProps.html.height
-        ? this.cachedProps.html.height : null;
-        this.body.style.overflow = this.cachedProps.body.overflow
-        ? this.cachedProps.body.overflow : null;
-      this.body.style.height = this.cachedProps.body.height
-        ? this.cachedProps.body.height : null;
+      scrollLock(false);
     }
   }
 
