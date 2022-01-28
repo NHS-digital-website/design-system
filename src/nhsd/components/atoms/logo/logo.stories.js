be4e0605bdb7f142847e72132e6e43c706a05b6b
Lee Jacobson <lee.jacobson1@nhs.net>
2022-01-28 10:35:53 +0000

    [DW-2798] NHS England and black logo variants

--- src/nhsd/components/atoms/logo/logo.stories.js
+++ src/nhsd/components/atoms/logo/logo.stories.js
@@ -5,7 +5,7 @@ import * as sbConfig from '../../../../../.storybook/storybook-config';
 import template from './template.njk';
 
 const componentName = 'Logo';
-const storyDescription = `${sbConfig.heading.lab}`;
+const storyDescription = `${sbConfig.heading.default}`;
 const sourceCode = `// Sass import \n@use "nhsd/components/atoms/logo";
 
 // HTML`;
@@ -25,11 +25,40 @@ export default {
 const Template = (args) => template.render({ params: { ...args } });
 
 export const LabComponent = Template.bind({});
-LabComponent.storyName = sbConfig.title.lab;
+LabComponent.storyName = sbConfig.title.defaultStory;
 LabComponent.parameters = {
   docs: {
     source: {
       code: `${sourceCode}\n${LabComponent()}`,
+    },
+  },
+};
+
+export const NHSEComponent = Template.bind({});
+NHSEComponent.storyName = 'NHS England';
+NHSEComponent.args = {
+  logo: 'nhse',
+};
+NHSEComponent.parameters = {
+  docs: {
+    source: {
+      code: `${sourceCode}\n${NHSEComponent(NHSEComponent.args)}`,
+    },
+  },
+};
+
+export const BlackLogoComponent = Template.bind({});
+BlackLogoComponent.storyName = 'Black Fill';
+BlackLogoComponent.args = {
+  classes: 'nhsd-a-logo--black',
+};
+BlackLogoComponent.parameters = {
+  docs: {
+    description: {
+      story: 'A black fill can be applied to the logo with the class `nhsd-a-logo--black`',
+    },
+    source: {
+      code: `${sourceCode}\n${BlackLogoComponent(BlackLogoComponent.args)}`,
     },
   },
 };
