fdee8f3a5ab98c31ff7c1fc0b0c29ec98b5ec98d
lekeDevOps <lesley.kelly2@nhs.net>
2023-11-29 11:23:20 +0000

    [DW-3497] Add descriptor and global header

---
+++ src/nhsd/components/atoms/descriptors/descriptors.stories.js
@@ -0,0 +1,35 @@
+// Load storybook config
+import * as sbConfig from '../../../../../.storybook/storybook-config';
+
+// Load template file
+import template from './template.njk';
+
+const componentName = 'Digital descriptor';
+const storyDescription = `${sbConfig.heading.lab}`;
+const sourceCode = `// Sass import \n@use "nhsd/components/atoms/descriptors";
+
+// HTML`;
+
+// Component defaults
+export default {
+  title: `${sbConfig.title.designSystem} / ${sbConfig.title.components} / ${sbConfig.title.atoms} / ${componentName}`,
+  parameters: {
+    docs: {
+      description: {
+        component: storyDescription,
+      },
+    },
+  },
+};
+
+const Template = (args) => template.render({ params: { ...args } });
+
+export const LabComponent = Template.bind({});
+LabComponent.storyName = sbConfig.title.lab;
+LabComponent.parameters = {
+  docs: {
+    source: {
+      code: `${sourceCode}\n${LabComponent()}`,
+    },
+  },
+};
